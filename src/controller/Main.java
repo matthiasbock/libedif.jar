b7007c926190c6524b89ba91283015265eba0bb3
Matthias <mail@matthiasbock.net>
2016-07-05 17:08:52 +0200

    begin with empty project

---
+++ src/controller/Main.java
@@ -0,0 +1,13 @@
+package controller;
+
+/**
+ * Open EDIF netlist file from Lattice Semiconductor's iCEcube2,
+ * parse it and export as JSON
+ */
+public class Main
+{
+	public static void main(String[] args)
+	{
+	    System.out.println("Hello world!");
+	}
+}
